0fae690a75fea0bc1ba8208f326629b2980e7541
Shivangkukreti <shivangkukretiwork@gmail.com>
2026-01-21 21:26:37 +0530

    ll

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const Schema=mongoose.Schema
 let sch=new Schema({
     _id:{type:String,required:true},
     email:{type:String,required:true},
-    username:{type:String,required:true},
+    name:{type:String,required:true},
     image:{type:String,required:true},
 })
 
